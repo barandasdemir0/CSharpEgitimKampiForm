9131f6dc3898106c2f209f36a008308b89271171
Your Name <barandasdemir.bd@gmail.com>
2025-07-02 17:08:12 +0300

    entity_framework_work

--- CSharpEgitimKampiForm.EFProject/Form1.cs
+++ CSharpEgitimKampiForm.EFProject/Form1.cs
@@ -16,5 +16,51 @@ namespace CSharpEgitimKampiForm.EFProject
         {
             InitializeComponent();
         }
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+
+            var values = db.Guides.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide = new Guide();
+            guide.GuideName = txtName.Text;
+            guide.GuideSurname = txtSurname.Text;
+            db.Guides.Add(guide);
+            db.SaveChanges();
+            MessageBox.Show("Başarıyla Eklendi");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var removeValue = db.Guides.Find(id);
+            db.Guides.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Başarıyla Silindi");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updateValue = db.Guides.Find(id);
+            updateValue.GuideName = txtName.Text;
+            updateValue.GuideSurname = txtSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Başarıyla Güncellendi");
+
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var value = db.Guides.Where(x => x.GuideId == id).ToList();
+            dataGridView1.DataSource = value;
+            
+           
+        }
     }
 }
